0c1ac1b0477b5372fe31048ff1a8440efc45b303
Alejandro <aleimedio@gmail.com>
2025-11-27 02:25:56 +0100

    Add HealthController with health check endpoint

---
+++ devsolaris-backend/src/main/java/springboot/devsolaris_backend/healthController/HealthController.java
@@ -0,0 +1,12 @@
+package springboot.devsolaris_backend.healthController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
